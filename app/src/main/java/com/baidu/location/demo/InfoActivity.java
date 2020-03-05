d4febe6db9f64a0db1cee9a485f811245de61d7a
hkl9 <hkl9@kent.ac.uk>
2020-03-05 10:10:41 +0000

    Updates on initial form

--- app/src/main/java/com/baidu/location/demo/InfoActivity.java
+++ app/src/main/java/com/baidu/location/demo/InfoActivity.java
@@ -4,18 +4,32 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.baidu.baidulocationdemo.R;
 
 public class InfoActivity extends Activity {
 
     private Button btnSubmit;
+    private Spinner sp_healthState;
+    private SeekBar sb_age;
+    private TextView tv_age;
+    private String[] sp_Item_HealthState = {"差劣", "略差", "一般", "良好", "優良"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+
+        sp_healthState = findViewById(R.id.sp_healthState);
+        ArrayAdapter aa_healthS = new ArrayAdapter(this, android.R.layout.simple_spinner_item, sp_Item_HealthState);
+        aa_healthS.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp_healthState.setAdapter(aa_healthS);
 
         btnSubmit = findViewById(R.id.btn_submit);
         btnSubmit.setOnClickListener(new View.OnClickListener() {
@@ -28,5 +42,22 @@ public class InfoActivity extends Activity {
             }
         });
 
+        sb_age = findViewById(R.id.sb_age);
+        sb_age.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tv_age.setText("" + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
