4f07e4b224824d3682b1235f37d142e86ab76743
Leo Wu <leowu4ever@users.noreply.github.com>
2020-03-10 13:37:19 +0000

    -ui updates

--- app/src/main/java/com/uk/location/activity/MyBroadcastReceiver.java
+++ app/src/main/java/com/uk/location/activity/MyBroadcastReceiver.java
@@ -1,10 +1,17 @@
 package com.uk.location.activity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class MyBroadcastReceiver extends BroadcastReceiver {
+
+    private AlarmManager am;
+    private Intent i;
+    private PendingIntent pi;
 
     public void onReceive(Context context, Intent intent) {
 
